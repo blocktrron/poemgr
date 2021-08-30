0a3d0c570fc54ef3c070e933def05e85849ffab0
David Bauer <mail@david-bauer.net>
2021-08-30 21:37:52 +0200

    poemgr: add configuration state and name to output

--- poemgr.c
+++ poemgr.c
@@ -184,11 +184,13 @@ int poemgr_show(struct poemgr_ctx *ctx)
 	for (int i = 0; i < ctx->profile->num_ports; i++) {
 		snprintf(port_idx, 3, "%d", i);
 		port_obj = json_object_new_object();
+		json_object_object_add(port_obj, "configured", json_object_new_boolean(!!ctx->ports[i].settings.name));
 		json_object_object_add(port_obj, "enabled", json_object_new_boolean(!!ctx->ports[i].status.enabled));
 		json_object_object_add(port_obj, "active", json_object_new_boolean(!!ctx->ports[i].status.active));
 		json_object_object_add(port_obj, "poe_class", json_object_new_int(ctx->ports[i].status.poe_class));
 		json_object_object_add(port_obj, "power", json_object_new_int(ctx->ports[i].status.power));
 		json_object_object_add(port_obj, "power_limit", json_object_new_int(ctx->ports[i].status.power_limit));
+		json_object_object_add(port_obj, "name", !!ctx->ports[i].settings.name ? json_object_new_string(ctx->ports[i].settings.name) : NULL);
 		json_object_object_add(port_obj, "faults", poemgr_create_port_fault_array(ctx->ports[i].status.faults));
 		/* ToDo: Export PSE specific data */
 
